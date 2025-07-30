fe82cde98c3e705b538f782f1468c5a7e41f98e8
Saeed Barari <barari.saeed.am@gmail.com>
2025-07-30 14:37:09 +0330

    migrated to CMake

--- HelloWorld/src/Math.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-static float lerp(float a, float b, float t)
-{
-    return b * t - a * (t - 1);
-}
---
+++ windows/src/entry.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <engine/app.hpp>
+
+int main()
+{
+    engine::entity entity;
+    std::cout << "Hello Cmake!" << entity.name << std::endl;
+}
